f49df4b9cf4acac7d8d35d5a798d47200b47f9df
Nasrddinovvv1508 <najmiddinnasriddinov5@gmail.com>
2024-06-09 20:51:33 +0500

    Vercel json

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non, ducimus quam, et culpa corrupti nesciunt aspernatur itaque ullam perspiciatis, suscipit commodi repudiandae quasi maxime qui velit exercitationem vitae a sunt?
+      </p>
+    </div>
   )
 }
 
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -2,13 +2,14 @@ import { useParams } from "react-router-dom";
 import { useFetch } from "../hooks/useFetch";
 
 function Product() {
-  let {id} = useParams();
-  let {data, setData, error} = useFetch(`https://dummyjson.com/products/${id}`)
+  let { id } = useParams();
+  let { data, isPending, error } = useFetch(`https://dummyjson.com/products/${id}`)
   console.log(data);
 
   return (
     <div>
-      <h1>Hello product {id}</h1>
+      <span>{isPending && <div className="load"><span className="loading loading-spinner loading-lg"></span></div>}</span>
+      <h1>{data && `Hello product ${id}`}</h1>
     </div>
   )
 }
