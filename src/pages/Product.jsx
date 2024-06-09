bd34dbf101b9bafd54cafe86a20df1c0a2c75267
Nasrddinovvv1508 <najmiddinnasriddinov5@gmail.com>
2024-06-09 22:14:08 +0500

    Products page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,15 +1,30 @@
+// hooks
 import { useParams } from "react-router-dom";
 import { useFetch } from "../hooks/useFetch";
+
+// icons
+import { FaCaretDown } from "react-icons/fa";
+
 
 function Product() {
   let { id } = useParams();
   let { data, isPending, error } = useFetch(`https://dummyjson.com/products/${id}`)
+
   console.log(data);
 
   return (
-    <div>
-      <span>{isPending && <div className="load"><span className="loading loading-spinner loading-lg"></span></div>}</span>
-      <h1>{data && `Hello product ${id}`}</h1>
+    <div className="mt-20">
+      <div>{isPending && <div className="load"><span className="loading loading-spinner loading-lg"></span></div>}</div>
+      <div className="flex justify-between items-center gap-20 mb-24">
+        <img className="w-1/2 border" src={data && data.thumbnail} alt="img" />
+        <div className="w-1/2">
+          <h1 className="text-5xl mb-7 font-semibold">{data && data.title}</h1>
+          <p className="opacity-60">
+            {data && data.description}
+          </p>
+          <button className="w-24 py-2 flex items-center justify-center gap-2 mt-24 text-blue-800">Down <FaCaretDown/></button>
+        </div>
+      </div>
     </div>
   )
 }
